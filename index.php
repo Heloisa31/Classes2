04d4222faff73a39580b97dfd90e4de822066d50
Heloisa31 <heloisa.souza.pinto@escola.pr.gov.br>
2025-05-29 07:21:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once "src/Classes2/Funcoes.php";
 //Instancia da classe
 $prod1 = new Funcoes;
 $prod1 -> definirProduto("Suco de uva");
-$prod1 -> definirPreco(5.50);
+$prod1 -> definirPreco(18.50);
 $prod1 -> definirQuantidade(5);
 $prod1 -> definirEstoque(20);
 $prod1 -> definirDescricao("Uma caixinha de suco de uva natural");
